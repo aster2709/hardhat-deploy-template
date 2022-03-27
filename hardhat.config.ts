04fa6b5eb7e2f8072e03dd27ff52f1d793e7436c
aster2709 <ymehta777@gmail.com>
2022-03-27 16:42:59 +0530

    feat: init template

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+import { HardhatUserConfig } from "hardhat/types";
+
+if (!process.env.DEPLOYER_PRIVATE_KEY) {
+  throw new Error("please input private key of deployer in .env file");
+}
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.13",
+  namedAccounts: { deployer: 0 },
+  networks: {
+    polygon_testnet: {
+      url: process.env.POLYGON_TESTNET_RPC,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+    },
+  },
+  verify: { etherscan: { apiKey: process.env.EXPLORER_API_KEY } },
+};
+
+export default config;
